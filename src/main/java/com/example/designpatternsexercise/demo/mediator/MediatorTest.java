b76552a7f8c84075998569646ec6189b9c1134cd
陈海明 <jxtc163@163.com>
2022-09-20 20:28:16 +0800

    中介模式

--- src/main/java/com/example/designpatternsexercise/demo/mediator/MediatorTest.java
+++ src/main/java/com/example/designpatternsexercise/demo/mediator/MediatorTest.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class MediatorTest {
     public static void main(String[] args) {
         System.out.println("小明想要看小区房和农民房");
-        Tenant2 xiaoMing = new Tenant2("小明",
-                Arrays.asList("XiaoQuFang", "NongMinFang"));
+        Tenant2 xiaoMing = new Tenant2("小明", Arrays.asList("XiaoQuFang", "NongMinFang"));
         xiaoMing.lookAtHouse();
     }
 }
